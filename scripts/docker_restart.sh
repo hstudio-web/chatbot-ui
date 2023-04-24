decccc9d8b5ccdc3e57d433e53f3886dad28196f
Rainy <houshuai0816@126.com>
2023-04-24 20:20:20 +0800

    fix: 网络变更为宿主模式

--- scripts/docker_restart.sh
+++ scripts/docker_restart.sh
@@ -2,6 +2,6 @@
 
 docker rm -f 51prompt
 docker rmi registry.cn-beijing.aliyuncs.com/hcloud_loverain/51prompt:v1.0.0
-docker run -d  -p 5000:3000 --name 51prompt-front registry.cn-beijing.aliyuncs.com/hcloud_loverain/51prompt:v1.0.0
+docker run -d  --net=host --name 51prompt-front registry.cn-beijing.aliyuncs.com/hcloud_loverain/51prompt:v1.0.0
 
 docker logs -f 51prompt-front --tail=20
