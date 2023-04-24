0dadc5563ad2751f42ee368c8667cfb73288e42a
Rainy <houshuai0816@126.com>
2023-04-24 21:17:40 +0800

    fix: 更新变量

--- scripts/docker_restart.sh
+++ scripts/docker_restart.sh
@@ -2,6 +2,6 @@
 
 docker rm -f 51prompt-front
 docker rmi registry.cn-beijing.aliyuncs.com/hcloud_loverain/51prompt:v1.0.0
-docker run -d  --net=host --name 51prompt-front registry.cn-beijing.aliyuncs.com/hcloud_loverain/51prompt:v1.0.0
+docker run -d  --net=host -e http_proxy=http://127.0.0.1:8118 -e https_proxy=http://127.0.0.1:8118 --name 51prompt-front registry.cn-beijing.aliyuncs.com/hcloud_loverain/51prompt:v1.0.0
 
 docker logs -f 51prompt-front --tail=20
